90c00250e37f657f15a56c32e7d57f5d99a94aca
komal paul <149079566+PaulWebdeveloper7@users.noreply.github.com>
2024-05-06 07:19:51 +0530

    hero section and navbar added

---
+++ constants/index.js
@@ -0,0 +1,128 @@
+// import { facebook, instagram, shieldTick, support, truckFast, twitter } from "@/assets/icons";
+const bigShoe1 = '/images/big-shoe1.png'
+const bigShoe2 = '/images/big-shoe2.png'
+const bigShoe3 = '/images/big-shoe3.png'
+const thumbnail1 = '/images/thumbnail-shoe1.svg'
+const thumbnail2 = '/images/thumbnail-shoe2.svg'
+const thumbnail3 = '/images/thumbnail-shoe3.svg'
+
+export const navLinks = [
+    { href: "#home", label: "Home" },
+    { href: "#about-us", label: "About Us" },
+    { href: "#products", label: "Products" },
+    { href: "#contact-us", label: "Contact Us" },
+];
+export const shoes = [
+    {
+        thumbnail: thumbnail1,
+        bigShoe: bigShoe1,
+    },
+    {
+        thumbnail: thumbnail2,
+        bigShoe: bigShoe2,
+    },
+    {
+        thumbnail: thumbnail3,
+        bigShoe: bigShoe3,
+    },
+];
+
+export const statistics = [
+    { value: '1k+', label: 'Brands' },
+    { value: '500+', label: 'Shops' },
+    { value: '250k+', label: 'Customers' },
+];
+
+// export const products = [
+//     {
+//         imgURL: shoe4,
+//         name: "Nike Air Jordan-01",
+//         price: "$200.20",
+//     },
+//     {
+//         imgURL: shoe5,
+//         name: "Nike Air Jordan-10",
+//         price: "$210.20",
+//     },
+//     {
+//         imgURL: shoe6,
+//         name: "Nike Air Jordan-100",
+//         price: "$220.20",
+//     },
+//     {
+//         imgURL: shoe7,
+//         name: "Nike Air Jordan-001",
+//         price: "$230.20",
+//     },
+// ];
+
+// export const services = [
+//     {
+//         imgURL: truckFast,
+//         label: "Free shipping",
+//         subtext: "Enjoy seamless shopping with our complimentary shipping service."
+//     },
+//     {
+//         imgURL: shieldTick,
+//         label: "Secure Payment",
+//         subtext: "Experience worry-free transactions with our secure payment options."
+//     },
+//     {
+//         imgURL: support,
+//         label: "Love to help you",
+//         subtext: "Our dedicated team is here to assist you every step of the way."
+//     },
+// ];
+
+// export const reviews = [
+//     {
+//         imgURL: customer1,
+//         customerName: 'Morich Brown',
+//         rating: 4.5,
+//         feedback: "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!"
+//     },
+//     {
+//         imgURL: customer2,
+//         customerName: 'Lota Mongeskar',
+//         rating: 4.5,
+//         feedback: "The product not only met but exceeded my expectations. I'll definitely be a returning customer!"
+//     }
+// ];
+
+
+// export const footerLinks = [
+//     {
+//         title: "Products",
+//         links: [
+//             { name: "Air Force 1", link: "/" },
+//             { name: "Air Max 1", link: "/" },
+//             { name: "Air Jordan 1", link: "/" },
+//             { name: "Air Force 2", link: "/" },
+//             { name: "Nike Waffle Racer", link: "/" },
+//             { name: "Nike Cortez", link: "/" },
+//         ],
+//     },
+//     {
+//         title: "Help",
+//         links: [
+//             { name: "About us", link: "/" },
+//             { name: "FAQs", link: "/" },
+//             { name: "How it works", link: "/" },
+//             { name: "Privacy policy", link: "/" },
+//             { name: "Payment policy", link: "/" },
+//         ],
+//     },
+//     {
+//         title: "Get in touch",
+//         links: [
+//             { name: "customer@nike.com", link: "mailto:customer@nike.com" },
+//             { name: "+92554862354", link: "tel:+92554862354" },
+//         ],
+//     },
+// ];
+
+// export const socialMedia = [
+//     { src: facebook, alt: "facebook logo" },
+//     { src: twitter, alt: "twitter logo" },
+//     { src: instagram, alt: "instagram logo" },
+// ];
